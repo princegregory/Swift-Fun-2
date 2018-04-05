20bd87f3bfd167a102a5312392373fd5f6dbb30d
Gregory Prince <gregoryprince@hotmail.com>
2018-04-05 00:53:38 -0400

    I did this in Kraken.
    I made a change.

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Swift Fun 2
+//
+//  Created by Gregory Prince on 4/5/18.
+//  Copyright © 2018 Gregory Prince. All rights reserved.
+//  Hello I like cheese
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var buttonCount = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
